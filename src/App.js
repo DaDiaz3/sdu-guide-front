76a1a44be210cb7aac42be6635f4a0968ff37312
Alen Raikan <mioprivate11@gmail.com>
2025-03-04 22:03:20 +0500

    Initialize project using Create React App
    # Conflicts:
    #	package-lock.json
    #	package.json
    #	src/App.js

--- src/App.js
+++ src/App.js
@@ -1,52 +1,24 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AdminWelcome from "./Components/AdminPanel/Welcome"; // Импортируем страницу
-import logo from "./logo.svg";
-import "./App.css";
-import { SignIn } from "./Components/AdminPanel/LoginPage";
-import { SignUp } from "./Components/AdminPanel/RegisterPage";
-import XlsxViewer from "./Components/AdminPanel/XlsxViwer/XlsxViewer";
-import RoomsList from "./Components/AdminPanel/RoomsPage";
-import ProfilePage from "./Components/AdminPanel/ProfilePage"
-import ProfileUpdatePage from "./Components/AdminPanel/ProfileEditPage";
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/admin" element={<AdminWelcome />} />
-          <Route path="/login" element={<SignIn/>} />
-          <Route path="/register" element={<SignUp/>} />
-          <Route path="/xlsxViewer/:hash" element={<XlsxViewer/>} />
-          <Route path="/rooms" element={<RoomsList/>} />
-          <Route path="/profile" element={<ProfilePage/>} />
-          <Route path="/update-user" element={<ProfileUpdatePage/>} />
-        </Routes>
-      </div>
-    </Router>
-  );
-}
-
-// Компонент главной страницы
-function Home() {
-  return (
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
