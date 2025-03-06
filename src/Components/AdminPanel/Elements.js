2854a76490a99e788d74576224b73f5809a3f8c0
Miras Kabykenov <m.kabykenov@origincreatives.com>
2025-03-06 16:22:17 +0500

    little styling login/register page

--- src/Components/AdminPanel/Elements.js
+++ src/Components/AdminPanel/Elements.js
@@ -6,6 +6,7 @@ export function Input({ type, placeholder, value, onChange }) {
         value={value}
         onChange={onChange}
         className="w-full p-2 border rounded-lg mb-2"
+        style={{height:"50px"}}
       />
     );
   }
@@ -15,6 +16,7 @@ export function Input({ type, placeholder, value, onChange }) {
       <button
         className="w-full bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700"
         onClick={onClick}
+        style={{backgroundColor:"#f0776c", height:"50px"}}
       >
         {children}
       </button>
